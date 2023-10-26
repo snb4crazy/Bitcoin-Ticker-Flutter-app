d72236a42c2b2a84ce5b526560bf4935b3cd7812
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-26 14:45:43 -0400

    CoinData: API request FUTURE method

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -32,6 +32,20 @@ const List<String> cryptoList = [
 ];
 
 const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+//todo never commit apiKey
 const apiKey = '40311912-TODO PUT TO ENV API-KEY';
 
-class CoinData {}
+class CoinData {
+  Future getCoinData() async {
+    String requestURL = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    http.Response response = await http.get(requestURL);
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'Failed requesting coin API';
+    }
+  }
+}
